4402e306ec49e361eb8e5a8e4e0db9bab3136967
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2023-07-25 15:39:52 +0100

    installed components and setup routes structure

---
+++ app/(dashboard)/(routes)/dashboard/pages.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+
+const DashboardPage = () => {
+  return (
+    <>
+      <p className="text-6xl text-green-500">Hello AI-SAAS</p>
+      <Button variant="destructive" size="sm">
+        Click Me
+      </Button>
+    </>
+  )
+}
+
+export default DashboardPage
