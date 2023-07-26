8eb96460adef3533a858dcc6372bae0868a5478a
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2023-07-26 13:17:16 +0100

    installed clark auth

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,7 @@
-import { Button } from '@/components/ui/button'
-
 const DashboardPage = () => {
   return (
     <>
       <p className="text-6xl text-green-500">Dashboard (Protected)</p>
-      <Button variant="destructive" size="sm">
-        Click Me
-      </Button>
     </>
   )
 }
