5c326f8d0d246d84b6651d6cf33b1c7315868832
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2023-08-03 14:23:13 +0100

    completed the dashboard page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,43 @@
-import { UserButton, SignedOut, SignInButton } from '@clerk/nextjs'
+'use client'
+
+import { Card } from '@/components/ui/card'
+import { cn } from '@/lib/utils'
+import { useRouter } from 'next/navigation'
+import { ArrowRight } from 'lucide-react'
+
+import { tools } from '@/constants'
 
 const DashboardPage = () => {
+  const router = useRouter()
+
   return (
-    <>
-      <p className="text-green-500">Dashboard (Protected)</p>
-    </>
+    <div>
+      <div className="my-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of AI
+        </h2>
+        <p className="text-muted-foreground font-light text-ms md:text-lg text-center">
+          Chat with the smartes AI - Experience the poer of AI
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn('p-2 w-fit rounded-md', tool.bgColor)}>
+                <tool.icon className={cn('w-8 h-8', tool.color)} />
+              </div>
+              <div className="font-semibold">{tool.label}</div>
+            </div>
+            <ArrowRight className="w-5 h-5" />
+          </Card>
+        ))}
+      </div>
+    </div>
   )
 }
 
